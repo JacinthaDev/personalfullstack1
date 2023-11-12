e8c0e9ec9d1bd85f5445dd5db61cc44ce35b7def
JacinthaDev <jacinthadev8@gmail.com>
2023-11-11 20:15:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# 📅 Appointment Scheduler
+
+
+
+# About this project
+This project is an advanced version of a previous repo called "savage-complete" that now includes authentication. Login to be taken to a message board to add 21 Savage lyrics.
+
+
+### View my project here: 
+
+##### Installation
+
+1. Clone repo
+2. run `npm install`
+
+##### Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:8080`
+
+
+
+## How It's Made:
+Tech used: JavaScript, Node.js. Express.js, EJS, MongoDB, Passport.js
+
+This is a full stack message board that allows you to enter a message, upvote, downvote, and delete your and other messages. You must make an account and log in to add to view and add to the message board.
+
+
+## Lessons Learned:
+I worked with passport.js the first time and learned more about requiring authentication from users. 
+
+## Examples:
+Take a look at similar projects!
+
+Packing List:
+
+Library Catalog:
+
+Appointment Scheduler:
+
+#### Backend Technologies
+MongoDB Atlas: https://www.mongodb.com/atlas/database
+
+
+
---
+++ public/main.js
@@ -0,0 +1,81 @@
+const edit = document.getElementsByClassName("fa-pen-to-square");
+const trash = document.getElementsByClassName("fa-trash");
+const updateButton = document.getElementById("update-button")
+
+updateButton.addEventListener('click', updateAppointment)
+
+//FreeCodeCamp for the line below
+let currentDate = new Date().toJSON().slice(0, 10)
+
+
+document.getElementById('first-date').setAttribute('min', currentDate)
+
+Array.from(edit).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const appointment = element.closest('li')
+        const id = appointment.childNodes[1].innerText
+        const name = appointment.childNodes[3].innerText
+        const reason = appointment.childNodes[5].innerText
+        const date = appointment.childNodes[9].innerText
+
+        document.getElementById('update-form').classList.toggle('hidden')
+        document.getElementById('update-id').value = id
+        document.getElementById('update-name').value = name
+        document.getElementById('update-reason').value = reason
+        document.getElementById('update-date').value = date
+        document.getElementById('update-date').setAttribute('min', currentDate)
+
+
+      });
+});
+
+
+function updateAppointment(event){
+  event.preventDefault()
+  event.stopPropagation() //stop the form from taking it's default action of GET and stop the submit button from submitting 
+
+  document.getElementById('update-form').classList.toggle('hidden')
+
+  fetch('appointments', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': document.getElementById('update-name').value,
+      'reason': document.getElementById('update-reason').value,
+      'date': document.getElementById('update-date').value,
+      'id': document.getElementById('update-id').value
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    window.location.reload(true)
+  })
+}
+
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const appointment = element.closest('li')
+        const id = appointment.childNodes[1].innerText
+        const name = appointment.childNodes[3].innerText
+        const reason = appointment.childNodes[5].innerText
+        const date = appointment.childNodes[7].innerText
+
+        fetch('appointments', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'reason': reason,
+            'date': date
+          })
+        }).then(function (response) {
+          appointment.remove()
+          window.location.reload()
+        })
+      });
+});
